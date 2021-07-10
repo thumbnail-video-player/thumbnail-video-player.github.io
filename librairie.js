3bebf8b0c79f1ebd482d67996332b6d45154e17c
Simon LANDAIS <slandais@a5sys.com>
2021-07-10 09:34:25 +0200

    Convert to lib and add 2 demo

--- librairie.js
+++ librairie.js
@@ -1,40 +1,31 @@
-function convertCodeToIframe(code) {
-    code = get_video_id(code);
+function ThumbnailVideoPlayer() {
 
-    var template = document.querySelector('#iframetemplate');
-    var clone = template.innerHTML;
-    clone = clone.replaceAll('CODEVIDEO', code);
+    var _this = this;
+    var config = {};
+    var templateHTML = `
+    <iframe
+        srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/CODEVIDEO?autoplay=1><img src=https://img.youtube.com/vi/CODEVIDEO/hqdefault.jpg alt='YouTube video player'><span>▶</span></a>"
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/CODEVIDEO"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen>
+    </iframe>`;
 
-    var textarea = document.querySelector('#textarea__content');
-    textarea.innerHTML = clone.replace(/</g,"&lt;");
-    // textarea.innerHTML = "<pre>" +
-    //     clone.replace(/</g,"&lt;") +
-    //     "</pre>";
+    this.convertCodeToIframe = (code) => {
+        code = this.get_video_id(code);
 
-    var apercu = document.querySelector('#apercu');
-    apercu.innerHTML  = clone;
-}
-
-function get_video_id(input) {
-    var regexMatch = input.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=|\/sandalsResorts#\w\/\w\/.*\/))([^\/&]{10,12})/);
-    if (regexMatch) {
-        return regexMatch[1];
-    } else {
-        return input;
+        return templateHTML.replaceAll('CODEVIDEO', code);
     }
-}
 
-function cody() {
-    /* Get the text field */
-    var copyText = document.getElementById('textarea__content');
-
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-    /* Copy the text inside the text field */
-    document.execCommand('copy');
-
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+    this.get_video_id = function(input) {
+        var regexMatch = input.match(/(?:youtu\.be\/|youtube\.com(?:\/embed\/|\/v\/|\/watch\?v=|\/user\/\S+|\/ytscreeningroom\?v=|\/sandalsResorts#\w\/\w\/.*\/))([^\/&]{10,12})/);
+        if (regexMatch) {
+            return regexMatch[1];
+        } else {
+            return input;
+        }
+    }
 }
--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 function convertToIframe(form) {
- convertCodeToIframe(form.code.value);
+ var thumbnailVideoPlayer = new ThumbnailVideoPlayer;
+ var iframe = thumbnailVideoPlayer.convertCodeToIframe(form.code.value);
+
+ var textarea = document.querySelector('#textarea__content');
+ textarea.innerHTML = iframe.replace(/</g,"&lt;");
+ // textarea.innerHTML = "<pre>" +
+ //     clone.replace(/</g,"&lt;") +
+ //     "</pre>";
+
+ var apercu = document.querySelector('#apercu');
+ apercu.innerHTML  = iframe;
+}
+
+function copy() {
+ /* Get the text field */
+ var copyText = document.getElementById('textarea__content');
+
+ /* Select the text field */
+ copyText.select();
+ copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+ /* Copy the text inside the text field */
+ document.execCommand('copy');
+
+ /* Alert the copied text */
+ alert("Copied the text: " + copyText.value);
 }
